76456efd8d713cc097c262b751486eaf25acb8b0
Hannah Jacoby <hjacoby@grubhub.com>
2019-10-28 16:24:49 -0600

    component add, config app.js

--- clicky-app/src/App.js
+++ clicky-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import MemoryCard from "./components/MemoryCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
 import memories from "./memories.json";
 
 class App extends Component {
@@ -8,13 +10,20 @@ class App extends Component {
     memories
   };
 
+  clickMemory = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    const memories = this.state.memories.filter(memory => memory.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ memories });
+  };
+
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Clicky Game</Title>
         {this.state.memories.map(memory => (
-          <FriendCard
+          <MemoryCard
             id={memory.id}
             key={memory.id}
             image={memory.image}
